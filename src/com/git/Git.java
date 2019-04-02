1d4e960c81c1771663e0f4a44a05a7b169dbd986
reddysravani <peddireddysravani8@gmail.com>
2019-04-02 10:07:02 +0530

    added by first user

--- src/com/git/Git.java
+++ src/com/git/Git.java
@@ -4,6 +4,8 @@ public class Git {
 
 	public static void main(String[] args) {
 		System.out.println("git first eclipse");
+		System.out.println("push from 1st user");
+		System.out.println("succesfully pushed");
 
 	}
 
--- src/com/git/git1.java
+++ src/com/git/git1.java
@@ -4,6 +4,7 @@ public class git1 {
 
 	public static void main(String[] args) {
 		System.out.println("second class 1eclipse");
+		System.out.println("pushing from 1st user in 2 class");
 	}
 
 }
