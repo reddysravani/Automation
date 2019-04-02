b065af493a2e476e2cd78718c7bdc5ce11ec0af9
reddysravani <peddireddysravani8@gmail.com>
2019-04-02 10:14:23 +0530

    added by second user

--- src/com/git/git1.java
+++ src/com/git/git1.java
@@ -5,6 +5,7 @@ public class git1 {
 	public static void main(String[] args) {
 		System.out.println("second class 1eclipse");
 		System.out.println("pushing from 1st user in 2 class");
+		System.out.println("pushing from second user");
 	}
 
 }
